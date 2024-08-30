23fee922b5ce1fa0d83cddca1578cc74fc6e0dd8
ahmetkoc <tr.ahmeetkocc@gmail.com>
2024-08-31 00:13:12 +0300

    [AuthAPI] User entity class and configurations created.

---
+++ AuthAPI/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthAPI.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; }
+        public string Role { get; set; } = string.Empty;
+    }
+}
