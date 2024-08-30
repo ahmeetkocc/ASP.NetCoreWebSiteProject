1596c3e2b40b5637d2225c18ee790f65b9cb6691
ahmetkoc <tr.ahmeetkocc@gmail.com>
2024-08-31 00:18:40 +0300

    [AuthAPI] DbSet created and configuration applied in DbContext.

--- AuthAPI/Data/AppDbContext.cs
+++ AuthAPI/Data/AppDbContext.cs
@@ -1,8 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using AuthAPI.Data.EntityConfigurations;
+using AuthAPI.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace AuthAPI.Data
 {
 	public class AppDbContext : DbContext
 	{
-	}
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfigurations());
+        }
+    }
+
+    
 }
