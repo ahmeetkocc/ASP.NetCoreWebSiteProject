08d0421daed55ad8985923842eff93fe7c8ffb37
ahmetkoc <tr.ahmeetkocc@gmail.com>
2024-08-31 16:39:33 +0300

    [AuthAPI] DI settings applied for DbContext.

--- AuthAPI/Program.cs
+++ AuthAPI/Program.cs
@@ -1,3 +1,6 @@
+using AuthAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AuthApiConStr")));
 
 var app = builder.Build();
 
@@ -15,6 +19,11 @@ if (app.Environment.IsDevelopment())
 	app.UseSwagger();
 	app.UseSwaggerUI();
 }
+
+using var scope = app.Services.CreateScope();
+var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+context.Database.EnsureDeleted();
+context.Database.EnsureCreated();
 
 app.UseHttpsRedirection();
 
