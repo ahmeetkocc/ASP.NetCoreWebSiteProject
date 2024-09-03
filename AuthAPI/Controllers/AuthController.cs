9e1ddb3759c3e9b22f5a96af4dd871a1c17fa58d
ahmetkoc <tr.ahmeetkocc@gmail.com>
2024-09-03 21:53:54 +0300

    [AuthAPI] Delete action added.

--- AuthAPI/Controllers/AuthController.cs
+++ AuthAPI/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using AuthAPI.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AuthAPI.Controllers
 {
@@ -27,6 +28,46 @@ namespace AuthAPI.Controllers
                 return BadRequest("Kullanıcı bulunamadi");
 
             return Ok(user);
+        }
+
+        //GetUserList
+        [HttpGet("UserList")]
+        public IActionResult GetUsers()
+        {
+            var userList = _appDbContext.Users
+                .Include(u => u.Role)
+                .Include(u => u.RefreshTokens)
+                .Select(u => new {
+                    u.Id,
+                    u.Username,
+                    u.Email,
+                    RoleName = u.Role.Name,
+                    u.RoleId,
+                    RefreshTokens = u.RefreshTokens.Select(x => x.Token)
+                })
+                .ToList();
+            return Ok(userList);
+        }
+
+        //DeleteUser
+        [HttpPost("delete/{userId}")]
+        public IActionResult Delete([FromRoute] int userId)
+        {
+            var user = _appDbContext.Users
+                .Include(u => u.Role)
+                .Include(u => u.RefreshTokens)
+                .SingleOrDefault(u => u.Id == userId);
+
+            if (user is null)
+                return NoContent();
+
+            //İlişkili yerlerin nasıl silineceği ile ilgili hata alındı.
+            _appDbContext.RefreshTokens.RemoveRange(user.RefreshTokens);
+
+            _appDbContext.Users.Remove(user);
+            _appDbContext.SaveChanges();
+
+            return Ok();
         }
     }
 }
