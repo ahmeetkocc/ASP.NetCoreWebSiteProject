32750799a9090b18e6b4b39a3ae2777531c4e7d6
ahmetkoc <tr.ahmeetkocc@gmail.com>
2024-09-08 20:26:27 +0300

    [AuthAPI] Version control.

--- AuthAPI/Program.cs
+++ AuthAPI/Program.cs
@@ -1,6 +1,9 @@
 using AuthAPI.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
