b3545c8dff470ab6d51c637fea6fd137672e1f77
ahmetkoc <tr.ahmeetkocc@gmail.com>
2024-09-08 20:21:22 +0300

    [AuthAPI] Jwt Auth configurations added.

--- AuthAPI/Program.cs
+++ AuthAPI/Program.cs
@@ -9,6 +9,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+                };
+                options.MapInboundClaims = true;
+            });
+
 builder.Services.AddSwaggerGen(c =>
 {
     c.AddSecurityDefinition(
