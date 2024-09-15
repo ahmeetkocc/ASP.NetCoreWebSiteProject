be4d011c5ce6c8fba6274ab8f257cc8fcf289b76
ahmetkoc <tr.ahmeetkocc@gmail.com>
2024-09-15 13:18:50 +0300

    [FileAPI] Upload action added.

---
+++ FileAPI/Controllers/FileController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FileController : ControllerBase
+{
+    [HttpPost("upload")]
+    public async Task<IActionResult> UploadFile(IFormFile file)
+    {
+        if (file == null || file.Length == 0)
+            return BadRequest("Invalid file");
+
+        var folderName = Path.Combine("Resources", "AllFiles");
+        var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), folderName);
+
+        if (!Directory.Exists(pathToSave))
+            Directory.CreateDirectory(pathToSave);
+
+        var fileName = file.FileName;
+        var fullPath = Path.Combine(pathToSave, fileName);
+        var dbPath = Path.Combine(folderName, fileName);
+
+        if (System.IO.File.Exists(fullPath))
+            return BadRequest("File already exists");
+
+        using (var stream = new FileStream(fullPath, FileMode.Create))
+        {
+            file.CopyTo(stream);
+        }
+
+        var urlToDownload = Url.Action("DownloadByName", "File", new { name = fileName }, Request.Scheme);
+
+        return Ok(new { Url = urlToDownload });
+    }
+}
