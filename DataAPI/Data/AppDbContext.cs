e4663f57057c5a62b503e9fb5d6fb2ede15fe3f2
ahmetkoc <tr.ahmeetkocc@gmail.com>
2024-09-19 19:56:24 +0300

    [DataAPI] AppDbContext created.

---
+++ DataAPI/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+    }
+}
