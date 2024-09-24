787871d277b2ccc411aa443113774d0b8b32c0dc
ahmetkoc <tr.ahmeetkocc@gmail.com>
2024-09-24 23:57:08 +0300

    [DataAPI] Remaining configuration files created.

---
+++ DataAPI/Data/EntityConfigurations/SkillConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using DataAPI.Data.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAPI.Data.EntityConfigurations
+{
+    public class SkillsConfigurations : IEntityTypeConfiguration<Skill>
+    {
+        public void Configure(EntityTypeBuilder<Skill> builder)
+        {
+            builder.HasKey(s => s.Id);
+            builder.Property(s => s.Id).ValueGeneratedOnAdd();
+
+            builder.Property(s => s.Name)
+                .IsRequired();
+
+        }
+    }
+}
