f968acdd3f2e28ba119d728dfcdf6d104125d631
ahmetkoc <tr.ahmeetkocc@gmail.com>
2024-11-06 23:14:27 +0300

    [feature_auth-service-refactor] Created service structure and moved business logic.

---
+++ AuthAPI/Dtos/GetUserResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthAPI.Dtos;
+
+public class GetUserResponseDto
+{
+	public int Id { get; set; }
+	public string Username { get; set; }
+	public string Email { get; set; }
+	public string RoleName { get; set; }
+	public int RoleId { get; set; }
+	public List<string> RefreshTokens { get; set; }
+}
